b9441ee777a4bccdfd14a79965ae10ca271bbaa4
Matheus Gomes <mth.gom@gmail.com>
2022-03-16 17:03:49 -0300

    Button changes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { ButtonProps } from 'react-native';
-
-import { Container, ButtonText } from './styles';
+import { Container, View } from './styles';
 
 interface ButtonProp extends ButtonProps {
   children: string;
 }
 
-const Button: React.FC<ButtonProp> = ({ children, ...rest }) => (
-  <Container {...rest}>
-    <ButtonText>{children}</ButtonText>
-  </Container>
+const Button: React.FC<ButtonProp> = ({ children, onPress }) => (
+  <View>
+    <Container
+      fullWidth={true}
+      onPress={onPress}
+      radius={10}
+      text={children}
+      type="flat"
+      style={Container}
+      textStyle={{
+        color: '#312e38',
+        fontFamily: 'RobotoSlab-Medium',
+        textTransform: 'capitalize',
+        fontSize: 18,
+      }}
+    />
+  </View>
 );
 
 export default Button;
+
+//{ color: '#213', fontFamily: 'RobotoSlab-Medium' }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,18 +1,13 @@
 import styled from 'styled-components/native';
+import { Button } from 'material-bread';
 
-export const Container = styled.TouchableOpacity`
+export const View = styled.View`
+  width: 100%;
+`;
+
+export const Container = styled(Button)`
   width: 100%;
   height: 60px;
   background: #ff9000;
-  border-radius: 10px;
   margin-top: 8px;
-
-  justify-content: center;
-  align-items: center;
-`;
-
-export const ButtonText = styled.Text`
-  font-family: 'RobotoSlab-Medium';
-  color: #312e38;
-  font-size: 18px;
 `;
