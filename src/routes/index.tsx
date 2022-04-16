cc91192aef2585c72be9ac5a21d32441fd9cafb6
Matheus Gomes <mth.gom@gmail.com>
2022-04-16 14:49:14 -0300

    Private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, ActivityIndicator } from 'react-native';
 
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+import AuthRoutes from './auth.routes';
+import { useAuth } from '../hooks/auth';
+import AppRoutes from './app.routes';
 
-const Stack = createNativeStackNavigator();
+const Routes: React.FC = () => {
+  const { user, loading } = useAuth();
 
-const AuthRoutes: React.FC = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerShown: false,
-      contentStyle: {
-        backgroundColor: '#312e38',
-      },
-    }}
-    initialRouteName="SignIn"
-  >
-    <Stack.Screen name="SignIn" component={SignIn} />
-    <Stack.Screen name="SignUp" component={SignUp} />
-  </Stack.Navigator>
-);
+  console.log(loading);
 
-export default AuthRoutes;
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#999" />
+      </View>
+    );
+  }
+
+  return user ? <AppRoutes /> : <AuthRoutes />;
+};
+
+export default Routes;
